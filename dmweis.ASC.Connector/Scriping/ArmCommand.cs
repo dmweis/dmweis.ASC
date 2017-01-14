7c0ac901af74b146ba80e2250d4585d5d3209f52
David Michael Weis <dweis7@gmail.com>
2017-01-14 23:46:54 +0100

    Fix serialization bug

--- dmweis.ASC.Connector/Scriping/ArmCommand.cs
+++ dmweis.ASC.Connector/Scriping/ArmCommand.cs
@@ -1,9 +1,13 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 using dmweis.ASC.Connector.Annotations;
 
 namespace dmweis.ASC.Connector.Scriping
 {
+   [XmlInclude(typeof(MoveCommand))]
+   [XmlInclude(typeof(MagnetCommand))]
+   [XmlInclude(typeof(DelayCommand))]
    public abstract class ArmCommand : INotifyPropertyChanged
    {
       public string Comment { get; set; }
